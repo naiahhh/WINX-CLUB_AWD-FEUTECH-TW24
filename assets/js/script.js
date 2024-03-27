600cacb08d5abe85890642a63a1bebb9a5a7464d
Eri Zanaiah <153974487+naiahhh@users.noreply.github.com>
2024-03-27 20:43:29 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,310 @@
-// side bar start
-
+// side bar start  
 function openNav() {
-    'use strict';
-    const sidepanel = document.getElementById('mySidepanel');
-    if(sidepanel){
-        sidepanel.style.left = '0';
-    } else{
-        console.error('error: sidepanel not found')
+    "use strict";
+    const sidepanel = document.getElementById("mySidepanel");
+    if (sidepanel) {
+        sidepanel.style.left = "0";
+    } else {
+        console.error("Error: Side panel element not found!");
     }
 }
 
 function closeNav() {
-    'use strict';
-    const sidepanel = document.getElementById('mySidepanel');
-    if(sidepanel){
-        sidepanel.style.left = '-320px';
-    } else{
-        console.error('error: sidepanel not found')
+    "use strict";
+    const sidepanel = document.getElementById("mySidepanel");
+    if (sidepanel) {
+        sidepanel.style.left = "-320px";
+    } else {
+        console.error("Error: Side panel element not found!");
     }
 }
 
-// searchbar
 
-function open_search() {
-    'use strict';
-    const searchpanel = document.getElementById('search-bar');
-    if(searchpanel){
-        searchpanel.style.height = '100vh';
-        searchpanel.style.borderRadius = '0';
-    } else{
-        console.error('error: searchpanel not found')
+function toggleCollapse(elementId) {
+    var element = document.getElementById(elementId);
+    var button = document.querySelector('.collapse_btn a');
+
+    // Toggle the 'show' class
+    element.classList.toggle('show');
+
+    // Toggle aria-expanded attribute
+    var isExpanded = button.getAttribute('aria-expanded') === 'true';
+    button.setAttribute('aria-expanded', !isExpanded);
+}
+
+// search-bar
+function open_search_bar() {
+    "use strict";
+    const sidepanel = document.getElementById("search-bar");
+    if (sidepanel) {
+        sidepanel.style.height = "100vh";
+        sidepanel.style.borderRadius = "0";
+    } else {
+        console.error("Error: Side panel element not found!");
     }
 }
 
-function close_search() {
-    'use strict';
-    const searchpanel = document.getElementById('search-bar');
-    if(searchpanel){
-        searchpanel.style.height = '0';
-        searchpanel.style.borderTopLeftRadius = '100%';
-        searchpanel.style.borderTopRightRadius = '100%';
-    } else{
-        console.error('error: searchpanel not found')
+// notification
+function open_notification() {
+    "use strict";
+    const sidepanel = document.getElementById("notification");
+    if (sidepanel) {
+        sidepanel.style.height = "100vh";
+        sidepanel.style.borderRadius = "0";
+    } else {
+        console.error("Error: Side panel element not found!");
     }
 }
-// responsive logoipsum slider
-$('.sliderlogo').slick{{
+
+function close_notification() {
+    "use strict";
+    const sidepanel = document.getElementById("notification");
+    if (sidepanel) {
+        sidepanel.style.height = "0";
+        sidepanel.style.borderTopLeftRadius = "100%";
+        sidepanel.style.borderTopRightRadius = "100%";
+    } else {
+        console.error("Error: Side panel element not found!");
+    }
+}
+
+// notification
+const bellIcon = document.getElementById('bellIcon');
+const notification = document.querySelector('.notification');
+
+bellIcon.addEventListener('click', () => {
+    const counter = Number(notification.getAttribute('data-count')) || 0;
+    notification.setAttribute('data-count', counter + 1);   
+    notification.classList.add('count');
+    notification.classList.add('notify');
+});
+
+notification.addEventListener('animationend', () => {
+    notification.classList.remove('notify');
+});
+
+// end notification
+
+function close_search_bar() {
+    "use strict";
+    const sidepanel = document.getElementById("search-bar");
+    if (sidepanel) {
+        sidepanel.style.height = "0";
+        sidepanel.style.borderTopLeftRadius = "100%";
+        sidepanel.style.borderTopRightRadius = "100%";
+    } else {
+        console.error("Error: Side panel element not found!");
+    }
+}
+
+// right-sidebar
+function open_right_side() {
+    "use strict";
+    const sidepanel = document.getElementById("right_side");
+    if (sidepanel) {
+        sidepanel.style.right = "0";
+    } else {
+        console.error("Error: Side panel element not found!");
+    }
+}
+
+function close_right_sade() {
+    "use strict";
+    const sidepanel = document.getElementById("right_side");
+    if (sidepanel) {
+        sidepanel.style.right = "-355px";
+    } else {
+        console.error("Error: Side panel element not found!");
+    }
+}
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("backToTopBtn").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    const scrollToTopBtn = document.documentElement || document.body;
+    scrollToTopBtn.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+
+
+// portfolio gallary tab
+function open_img(evt, cityName) {
+    var i, tabcontent, tablinks;
+
+    // Hide all tab content
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove active class from all tab links
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    // Show the selected tab content and mark the corresponding tab link as active
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
+
+
+
+
+
+
+// button back to top 
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("backToTopBtn").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    const scrollToTopBtn = document.documentElement || document.body;
+    scrollToTopBtn.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+
+// faq section
+document.addEventListener("DOMContentLoaded", function () {
+    let accordionButtons = document.querySelectorAll('.accordion-button');
+    let acoimg = document.querySelectorAll('.accordion-button img');
+
+    accordionButtons.forEach(function (button, index) {
+        button.addEventListener('click', function () {
+            let collapse = this.parentElement.nextElementSibling;
+
+            // Close all other accordion items
+            accordionButtons.forEach(function (otherButton, otherIndex) {
+                if (otherButton !== button) {
+                    var otherCollapse = otherButton.parentElement.nextElementSibling;
+                    otherCollapse.style.maxHeight = null;
+                    // Reset the image source and rotation for other accordion items
+                    acoimg[otherIndex].src = 'assets/img/icon/plus.png';
+                    acoimg[otherIndex].style.transform = 'rotate(0deg)';
+                    otherButton.style.backgroundColor = '#fff';
+                }
+            });
+
+            // Toggle the clicked accordion item
+            if (collapse.style.maxHeight) {
+                collapse.style.maxHeight = null;
+                // Reset the image source, rotation, and background color when collapsing
+                acoimg[index].src = 'assets/img/icon/plus.png';
+                acoimg[index].style.transform = 'rotate(90deg)';
+                button.style.backgroundColor = '';
+            } else {
+                collapse.style.maxHeight = collapse.scrollHeight + "px";
+                // Change the image source, set rotation, and background color when expanding
+                acoimg[index].src = 'assets/img/icon/menu.png';
+                acoimg[index].style.transform = 'rotate(180deg)';
+                button.style.backgroundColor = '#c1b0d5';
+            }
+        });
+    });
+});
+
+
+
+
+
+// footer validation start
+const fom = document.getElementById('footer-form');
+const footerMessage = document.getElementById('footer-message');
+
+fom.addEventListener('submit', (event) => {
+    event.preventDefault();
+    footerMessage.innerHTML = '~ Form submitted success fully!';
+    footerMessage.style.display = 'flex';
+    fom.reset();
+    setTimeout(() => {
+        footerMessage.style.display = 'none';
+    }, 3000);
+});
+// footer validation end
+
+
+
+
+
+// responsive Logoipsum Slider
+$('.sliderlogo').slick({
     arrows: false,
     dots: false,
     infinite: false,
     autoplay: false,
     speed: 300,
     slidesToShow: 5,
-    sliderToScroll: 1,
-    responsive:{{
-        breakpoint: 1024,
-        settings:{
-            : 4,
-            sliderToScroll: 1
-            infinite: true,
-            dots: false,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+    ]
+});
+
+
+// responsive team Slider
+$('.team-slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: false,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-    },
-{
-    breakpoint: 600,
-    settings:{
-        : 2,
-        sliderToScroll: 1
-    }
-},
-{
-    breakpoint: 480,
-    settings:{
-        : 2,
-        sliderToScroll: 1
-}
-}
-}
+    ]
+});
