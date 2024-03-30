22783b88e4b297068fef66ef7e19ca8e147afdf2
Dalet <104555136+Spod101@users.noreply.github.com>
2024-03-31 00:12:47 +0800

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,7 +149,17 @@ function logout() {
     window.location.href = './index.html';
 }
 
+if (!localStorage.getItem('username')) {
+    document.getElementById('logout').style.display = 'none';
+}
+
+function logout() {
+    localStorage.removeItem('username');
+    window.location.href = './index.html';
+}
+
 document.getElementById('_logout').addEventListener('click', logout);
+document.getElementById('logout').addEventListener('click', logout);
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
