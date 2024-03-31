d2d76611169bd1f933691d7345285f0a37ab03b7
alingetz <allyza.goyon@gmail.com>
2024-04-01 02:59:38 +0800

    UPD LAST NA SER

--- assets/js/form.js
+++ assets/js/form.js
@@ -77,12 +77,12 @@ document.querySelectorAll('.show-password-label').forEach(function(label) {
   eyeIcon.addEventListener('click', function() {
       if (passwordInput.type === 'password') {
           passwordInput.type = 'text';
-          eyeIcon.classList.remove('fa-eye');
-          eyeIcon.classList.add('fa-eye-slash');
-      } else {
-          passwordInput.type = 'password';
           eyeIcon.classList.remove('fa-eye-slash');
           eyeIcon.classList.add('fa-eye');
+      } else {
+          passwordInput.type = 'password';
+          eyeIcon.classList.remove('fa-eye');
+          eyeIcon.classList.add('fa-eye-slash');
       }
   });
 });
